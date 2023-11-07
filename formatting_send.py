1e8cba52dd03025d34eb8190f87fed07d684ab24
Jyoti <jyotisingh82000@gmail.com>
2023-11-07 13:49:49 +0530

    import requests and funtioning

--- formatting_send.py
+++ formatting_send.py
@@ -1,8 +1,30 @@
 from formatting import format_msg
-def send (name, website = None):
-    msg = format_msg(my_name=name,my_website=website)
-    print(msg)
-    return msg
+from requests import get
+# import sys
 
+def send (name, website = None, verbose= False):
+    if website != None:
+        msg = format_msg(my_name=name, my_website=website)
+        
+    else:
+        msg = format_msg(my_name = name)
+        
+    if verbose:
+        print(name, website)
+    r = get("https://httpbin.org/json")
+    
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return "there was an error"
+    
 
-send("jyoti","cfe")
+if __name__ == "__main__":  
+    # print(sys.argv)
+    # name = 
+    response= send("jyoti","cfe", verbose=True)
+
+    print(response)
+
+# print(r.json())
+# print(r.status_code)
